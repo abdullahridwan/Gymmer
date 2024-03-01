c7ecbdf3b00ad77b0a8050bddee1145c5504cc58
Abdullah Ridwan <53100755+abdullahridwan@users.noreply.github.com>
2024-02-29 21:09:39 -0500

    Initial Commit

---
+++ Gymmer/GymmerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymmerApp.swift
+//  Gymmer
+//
+//  Created by Abdullah Ridwan on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct GymmerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
